9830a89b687bf6c30c00efdc1267de8ea06a263d
HongJoker <903358069@qq.com>
2021-05-18 20:41:20 +0800

    Initial commit

---
+++ src/Data_base.java
@@ -0,0 +1,20 @@
+public class Data_base {
+    short Short;
+    int Int;
+    byte BYTE;
+    long LONG;
+
+    float FLOAT;
+    Double DOUBLE;
+
+    Boolean TRUE;
+    Boolean FALSE;
+
+    //变量命名规则
+    /*
+    * 1、只能以字母、数字、下划线、￥符合组成
+    * 2、不能以数字开头
+    * 3、名称中不能使用JAVA中的关键字
+    * */
+
+}
